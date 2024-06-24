411785d3ce9245e5762a9fdcbf3876b6e65cfd53
SimonGideon <simongideon918@gmail.com>
2024-06-24 11:31:16 +0300

    Add book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_13_101713) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_081612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.string "language", null: false
+    t.string "edition"
+    t.string "publisher"
+    t.integer "page_count", null: false
+    t.date "published_at", null: false
+    t.string "isbn"
+    t.boolean "featured", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
