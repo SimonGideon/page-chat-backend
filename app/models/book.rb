04b5a4e60bad1e20127622ad6c077b4928f21860
SimonGideon <simongideon918@gmail.com>
2024-06-24 11:50:23 +0300

    Add books validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  has_one_attached :pdf
+  has_one_attached :cover_image
+  validates :title, :description, :language, :page_count, :published_at, presence: true
+  validates :featured, inclusion: { in: [true, false] }
+  validates :pdf, attached: true, content_type: ["application/pdf"]
+  validates :cover_image, attached: true, content_type: ["image/png", "image/jpg", "image/jpeg", "image/avif", "image/webp", "image/svg+xml"]
 end
