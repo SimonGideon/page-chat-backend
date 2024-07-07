2438b43802fb3601fe2adc1bdc8ceb959ac5462f
SimonGideon <simongideon918@gmail.com>
2024-07-07 21:44:43 +0300

    Extract page count from pdf

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,56 +4,51 @@ class Book < ApplicationRecord
 
   validates :title, :description, :language, :published_at, presence: true
   validates :featured, inclusion: { in: [true, false] }
-  # validate :only_pdf
-  # validate :acceptable_images
-  # before_save :extract_page_count
-  # before_save :extract_cover_image, if: -> { pdf.attached? && !cover_image.attached? }
+  validate :only_pdf
+  validate :acceptable_images
+
+  after_commit :extract_page_count, if: :pdf_attached?
+
   private
 
   # PDF upload content-type validation
   def only_pdf
-    # return unless pdf.attached?
+    return unless pdf.attached?
 
     unless pdf.content_type == "application/pdf"
-      errors.add(:pdf, "must be a PDF")
+      errors.add(:pdf, "Book must be a PDF")
     end
   end
 
-  # # Cover image upload validation
-  # def acceptable_images
-  #   return unless cover_image.attached?
+  # Cover image upload validation
+  def acceptable_images
+    return unless cover_image.attached?
 
-  #   unless cover_image.byte_size <= 1.megabyte
-  #     errors.add(:cover_image, "is too big")
-  #   end
+    unless cover_image.byte_size <= 1.megabyte
+      errors.add(:cover_image, "is too big")
+    end
 
-  #   acceptable_types = ["image/png", "image/jpg", "image/jpeg", "image/avif", "image/webp", "image/svg+xml"]
-  #   unless acceptable_types.include?(cover_image.content_type)
-  #     errors.add(:cover_image, "must be a JPEG, JPG, AVIF, WEBP, SVG, or PNG")
-  #   end
-  # end
+    acceptable_types = ["image/png", "image/jpg", "image/jpeg", "image/avif", "image/webp", "image/svg+xml"]
+    unless acceptable_types.include?(cover_image.content_type)
+      errors.add(:cover_image, "must be a JPEG, JPG, AVIF, WEBP, SVG, or PNG")
+    end
+  end
+
+  # extract first page covert to image as cover image
+  def extract_cover_image
+    return unless pdf.attached?
+  end
 
   # Extract page count from attached PDF
-  # def extract_page_count
-  #   # return unless pdf.attached?
+  def extract_page_count
+    return unless pdf.attached?
 
-  #   reader = PDF::Reader.new(pdf.download)
-  #   self.page_count = reader.page_count
-  # end
+    reader = PDF::Reader.new(StringIO.new(pdf.download))
+    self.update_column(:page_count, reader.page_count)
+  end
 
-  # Extract cover image from attached PDF and attach it
-  # def extract_cover_image
-  #   return unless pdf.attached?
-
-  #   reader = PDF::Reader.new(pdf.download)
-  #   # Extract the first image from the first page of the PDF
-  #   page = reader.pages.first
-  #   image = page&.images&.first
-
-  #   return unless image
-
-  #   # Attach the extracted image as the cover image
-  #   cover_image.attach(io: StringIO.new(image.data), filename: "cover_image.jpg", content_type: "image/jpeg")
-  # end
-
+  # Check if the PDF is attached
+  def pdf_attached?
+    pdf.attached?
+  end
 end
