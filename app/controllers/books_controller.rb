e47b6ed97775e5b353b321846b027841b6621206
SimonGideon <simongideon918@gmail.com>
2024-07-31 23:01:41 +0300

    feat: exposed featured books endpoint

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show recommended]
+  before_action :authenticate_user!, except: %i[ recommended featured ]
   before_action :set_book, only: %i[show update destroy]
 
   # GET /books
