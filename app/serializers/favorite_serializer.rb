2c821d04d9b15e8c147040a5c6b10fb6d2a126a6
SimonGideon <simongideon918@gmail.com>
2024-08-11 01:54:24 +0300

    refactor: favorite serializer to capture related book

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -2,5 +2,5 @@ class FavoriteSerializer
   include JSONAPI::Serializer
   attributes :id, :user, :book
 
-  # belongs_to :book, serializer: BookSerializer
+  belongs_to :book, serializer: BookSerializer
 end
