3ec4b2f223bdfde7a643260c4a8257e985ffe3ac
Simon Gideon <simongideon918@gmail.com>
2025-11-25 13:10:06 +0300

    Intial commit

--- config/initializers/rswag_api.rb
+++ config/initializers/rswag_api.rb
@@ -1,5 +1,5 @@
 Rswag::Api.configure do |c|
-  c.swagger_root = Rails.root.join("swagger").to_s
+  c.openapi_root = Rails.root.join("swagger").to_s
 
   # This allows you to dynamically filter the swagger docs per request if needed.
   c.swagger_filter = lambda do |swagger, env|
--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,4 +1,4 @@
 Rswag::Ui.configure do |c|
-  c.swagger_endpoint "/api-docs/v1/swagger.yaml", "Page Chat API V1"
+  c.openapi_endpoint "/api-docs/v1/swagger.yaml", "Page Chat API V1"
 end
 
