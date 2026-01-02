d3e5fddd0ba17760036e6fd977c8091d9281662d
Simon Gideon <simongideon918@gmail.com>
2026-01-02 13:57:37 +0300

    Enhance author attribute in BookSerializer
    The author attribute now returns a detailed hash including id, name, biography, email, avatar_url, and social_handles, instead of just a value. This provides richer author information in serialized book responses.

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -3,7 +3,18 @@ class BookSerializer
   include JSONAPI::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :title, :description, :language, :publisher, :page_count, :isbn, :featured, :recommended, :published_at, :pdf_url, :cover_image_url, :published_date, :author, :category
+  attributes :id, :title, :description, :language, :publisher, :page_count, :isbn, :featured, :recommended, :published_at, :pdf_url, :cover_image_url, :published_date, :category
+
+  attribute :author do |book|
+    {
+      id: book.author.id,
+      name: book.author.name,
+      biography: book.author.biography,
+      email: book.author.email,
+      avatar_url: book.author.avatar.attached? ? Rails.application.routes.url_helpers.rails_blob_path(book.author.avatar, only_path: true) : nil,
+      social_handles: book.author.social_handles
+    }
+  end
 
   attribute :pdf_url do |book, params|
     if params[:current_user] && book.pdf.attached?
